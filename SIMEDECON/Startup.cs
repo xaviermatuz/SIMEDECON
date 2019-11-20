deccb609dec0edcb3577a8802807334b4a96ed10
xavi <jemw7991@yahoo.es>
2019-11-20 09:24:13 -0600

    Archivos base

---
+++ SIMEDECON/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIMEDECON.Startup))]
+namespace SIMEDECON
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
