2171c481ab9e52fc2897ee5d3938420b53be97a6
xavi <jemw7991@yahoo.es>
2019-11-25 11:22:26 -0600

    ya me chicle JAV

--- SIMEDECON.Tests/Controllers/HomeControllerTest.cs
+++ SIMEDECON.Tests/Controllers/HomeControllerTest.cs
@@ -13,30 +13,42 @@ namespace SIMEDECON.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void ViewAdministrador()
+        public void Index()
         {
             // Arrange
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.ViewAdministrador() as ViewResult;
+            ViewResult result = controller.Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
-        public void ViewMedico()
+        public void About()
         {
             // Arrange
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.ViewMedico() as ViewResult;
+            ViewResult result = controller.About() as ViewResult;
 
             // Assert
             Assert.AreEqual("Your application description page.", result.ViewBag.Message);
         }
 
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
