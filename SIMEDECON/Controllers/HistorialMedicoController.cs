00e042866dfd4614d297c88ff8d3b1c32c3cf7cc
xavi <jemw7991@yahoo.es>
2019-12-05 15:19:15 -0600

    Update de Bootstrap y agregado del SiteMap desarrollo pendente de layout

---
+++ SIMEDECON/Controllers/HistorialMedicoController.cs
@@ -0,0 +1,34 @@
+﻿using SIMEDECON.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIMEDECON.Controllers
+{
+    public class HistorialMedicoController : Controller
+    {
+        public MeSysEntities db = new MeSysEntities();
+        // GET: HistorialMedico
+        public Datos_Atleta GetDA(int ID)
+        {
+            Datos_Atleta model = new Datos_Atleta();
+            Datos_Atleta da = db.Datos_Atleta.Find(ID);
+            model.ID = da.ID;
+            model.Nombre_Completo = da.Nombre_Completo;
+            ViewBag.Nombre = model.Nombre_Completo;
+            return model;
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Actualizar()
+        {
+            return View();
+        }
+
+    }
+}
