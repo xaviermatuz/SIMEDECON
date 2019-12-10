fd956c432eb9a28cc5402e212e0d5e529d37996b
xavi <jemw7991@yahoo.es>
2019-12-10 07:04:25 -0600

    Sincronizar

---
+++ SIMEDECON/Controllers/AtletaController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SIMEDECON.Models;
+using SIMEDECON.Models.ViewModel;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIMEDECON.Controllers
+{
+    public class AtletaController : Controller
+    {
+        DAL objdal = new DAL();
+        MeSysEntities ME = new MeSysEntities();//LLamada a Entityframework, para seleccion de tabla
+        // GET: Atleta
+        public ActionResult Index()
+        {
+
+            IEnumerable<Datos_Atleta> Atleta = ME.Datos_Atleta.Where(s => s.Activo.ToString() == "true");
+            ViewBag.Atleta = Atleta;
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Actualizar()
+        {
+            return View();
+        }
+        public ActionResult Mostrar()
+        {           
+            return View();
+        }
+
+        //POST
+
+    }
+}
---
+++ SIMEDECON/Controllers/DAL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIMEDECON.Controllers
+{
+    public class DAL
+    {
+        DataSet ds;
+        SqlDataAdapter da;
+
+        public static SqlConnection connect()
+        {
+            //Reading the connection string from web.config    
+            string Name = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+            //Passing the string in sqlconnection.    
+            SqlConnection con = new SqlConnection(Name);
+            //Check wheather the connection is close or not if open close it else open it    
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+
+            }
+            else
+            {
+
+                con.Open();
+            }
+            return con;
+
+        }
+        //Creating a method which accept any type of query from controller to execute and give result.    
+        //result kept in datatable and send back to the controller.    
+        public DataTable MyMethod(string Query)
+        {
+            ds = new DataSet();
+            DataTable dt = new DataTable();
+            da = new SqlDataAdapter(Query, DAL.connect());
+
+            da.Fill(dt);
+            List<SelectListItem> list = new List<SelectListItem>();
+            List<SelectListItem> list1 = new List<SelectListItem>();
+            List<SelectListItem> list2 = new List<SelectListItem>();
+            return dt;
+
+        }
+    }
+}
